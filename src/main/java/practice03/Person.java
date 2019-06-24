b36a2514809ca595628b16c76fc19fe596bd1a83
yaeli <yaeli@thoughtworks.com>
2019-06-24 13:09:56 +0800

    [Yae][Practice3 - create Work class extends from Person and override introduce method in Student and Person]

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,23 @@
 package practice03;
 
 public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". " +  "I am " +  age + " years old.";
+    }
 }
