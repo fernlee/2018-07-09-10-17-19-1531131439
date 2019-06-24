259df0dfcf407d3591f94fdee26e3ad14827cece
yaeli <yaeli@thoughtworks.com>
2019-06-24 13:46:26 +0800

    [Yae][Practice4 - extract basicIntroduce function]

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,27 @@
 package practice04;
 
 public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected String basicIntroduce() {
+        return "My name is " + name + ". " +  "I am " +  age + " years old.";
+    }
+
+    public String introduce() {
+        return basicIntroduce();
+    }
 }
