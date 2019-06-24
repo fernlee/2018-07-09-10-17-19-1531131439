1d6c3018388f9358c267d1399c6d89d4dd8cb71a
yaeli <yaeli@thoughtworks.com>
2019-06-24 13:53:46 +0800

    [Yae][Practice6 - implement Teacher class]

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,19 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    private Integer klass;
+
+    public Student(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a Student. I am at Class " + klass + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a Teacher. I teach " + (klass != null ? "Class " + klass : "No Class") + ".";
+    }
 }
