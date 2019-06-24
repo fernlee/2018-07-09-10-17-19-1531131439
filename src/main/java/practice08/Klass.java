2eabe15a88a4e45897ced381fe03b10bb993a25c
yaeli <yaeli@thoughtworks.com>
2019-06-24 15:00:19 +0800

    [Yae][Practice8 - add id property in Person and leader in Klass]

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,27 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (klass != null ? klass.getDisplayName() : "No Class") + ".";
+    }
+
+    public String introduceWith(Student student) {
+        return super.introduce() + " I am a Teacher. " + (student.getKlass().getNumber() == klass.getNumber() ? "I teach " : "I don't teach ") + student.getName()+".";
+    }
 }
