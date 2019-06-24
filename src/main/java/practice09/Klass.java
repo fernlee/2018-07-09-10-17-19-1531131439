9e784add9c5445e406cc65f3cb6441356ea07b48
yaeli <yaeli@thoughtworks.com>
2019-06-24 15:12:13 +0800

    [Yae][Practice9 - implement appendMember method in Klass]

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,38 @@
 package practice09;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private ArrayList<Student> students;
+
+    public Klass(Integer number) {
+        this.number = number;
+        this.students = new ArrayList<>();
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.indexOf(leader) > -1) {
+            this.leader = leader;
+        } else {
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,19 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a Student. I am " + (this.equals(klass.getLeader()) ? "Leader of ": "at ") + klass.getDisplayName() + ".";
+    }
 }
